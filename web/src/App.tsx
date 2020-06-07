abaf7c40817a62fb75a8470bfd1c32ec76226537
victor thury <victorhauache@gmail.com>
2020-06-07 14:09:50 -0400

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+// JSX: Sintaxe de XML dentro do Javascript
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
